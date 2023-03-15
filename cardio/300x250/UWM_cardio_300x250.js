a9193050a36167080793d695c060c2df8de4eecb
ApprovalPage <curtisjackson23@gmail.com>
2023-03-15 14:35:26 -0700

    Add files via upload

--- cardio/300x250/UWM_cardio_300x250.js
+++ cardio/300x250/UWM_cardio_300x250.js
@@ -464,6 +464,29 @@ if (reversed == null) { reversed = false; }
 }).prototype = getMCSymbolPrototype(lib.CTAskewbox, new cjs.Rectangle(-356,-60.4,712,124.9), null);
 
 
+(lib.border = function(mode,startPosition,loop,reversed) {
+if (loop == null) { loop = true; }
+if (reversed == null) { reversed = false; }
+	var props = new Object();
+	props.mode = mode;
+	props.startPosition = startPosition;
+	props.labels = {};
+	props.loop = loop;
+	props.reversed = reversed;
+	cjs.MovieClip.apply(this,[props]);
+
+	// Layer_1
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f().s("#000000").ss(1,0,0,3).p("AXXp1IAATmMgutAAAIAAzm");
+	this.shape.setTransform(0,0.275);
+
+	this.timeline.addTween(cjs.Tween.get(this.shape).wait(1));
+
+	this._renderFirstFrame();
+
+}).prototype = getMCSymbolPrototype(lib.border, new cjs.Rectangle(-150.5,-63.7,301,127.5), null);
+
+
 (lib.BKGDskewbox = function(mode,startPosition,loop,reversed) {
 if (loop == null) { loop = true; }
 if (reversed == null) { reversed = false; }
@@ -526,13 +549,6 @@ if (reversed == null) { reversed = false; }
 	// actions tween:
 	this.timeline.addTween(cjs.Tween.get(this).call(this.frame_0).wait(283).call(this.frame_283).wait(1));
 
-	// border
-	this.shape = new cjs.Shape();
-	this.shape.graphics.f().s("#000000").ss(1,1,1).p("A3WzcMAutAAAMAAAAm5MgutAAAg");
-	this.shape.setTransform(150,125);
-
-	this.timeline.addTween(cjs.Tween.get(this.shape).wait(284));
-
 	// UWMed logo
 	this.instance = new lib.UWMed_logo();
 	this.instance.setTransform(150.75,287.3,0.75,0.75,0,0,0,0.4,0.4);
@@ -548,61 +564,69 @@ if (reversed == null) { reversed = false; }
 
 	this.timeline.addTween(cjs.Tween.get(this.instance_1).wait(248).to({_off:false},0).to({x:150,alpha:1},23,cjs.Ease.quintOut).wait(13));
 
-	// HEAD2
-	this.instance_2 = new lib.HEAD2();
-	this.instance_2.setTransform(246,207);
+	// CTA skew box
+	this.instance_2 = new lib.CTAskewbox();
+	this.instance_2.setTransform(200,161,1,1,0,0,0,50,0);
 	this.instance_2.alpha = 0;
 	this.instance_2._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_2).wait(119).to({_off:false},0).to({x:146,alpha:1},23,cjs.Ease.quintOut).wait(87).to({y:247,alpha:0},9,cjs.Ease.quintIn).to({_off:true},1).wait(45));
+	this.timeline.addTween(cjs.Tween.get(this.instance_2).wait(246).to({_off:false},0).to({regY:0.1,scaleY:1.0092,y:61.1,alpha:1},23,cjs.Ease.quintOut).wait(15));
+
+	// border
+	this.instance_3 = new lib.border();
+	this.instance_3.setTransform(150,186.7);
+	this.instance_3.alpha = 0;
+	this.instance_3._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_3).wait(248).to({_off:false},0).to({alpha:1},21).wait(15));
+
+	// HEAD2
+	this.instance_4 = new lib.HEAD2();
+	this.instance_4.setTransform(246,207);
+	this.instance_4.alpha = 0;
+	this.instance_4._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_4).wait(119).to({_off:false},0).to({x:146,alpha:1},23,cjs.Ease.quintOut).wait(87).to({y:247,alpha:0},9,cjs.Ease.quintIn).to({_off:true},1).wait(45));
 
 	// HEAD1
-	this.instance_3 = new lib.HEAD1();
-	this.instance_3.setTransform(250,207);
-	this.instance_3.alpha = 0;
+	this.instance_5 = new lib.HEAD1();
+	this.instance_5.setTransform(250,207);
+	this.instance_5.alpha = 0;
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_3).to({x:150,alpha:1},22,cjs.Ease.quintOut).wait(87).to({x:240,alpha:0},9,cjs.Ease.quintIn).to({_off:true},1).wait(165));
+	this.timeline.addTween(cjs.Tween.get(this.instance_5).to({x:150,alpha:1},22,cjs.Ease.quintOut).wait(87).to({x:240,alpha:0},9,cjs.Ease.quintIn).to({_off:true},1).wait(165));
 
 	// HEAD skew box
-	this.instance_4 = new lib.HEADskewbox();
-	this.instance_4.setTransform(136,225);
+	this.instance_6 = new lib.HEADskewbox();
+	this.instance_6.setTransform(136,225);
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_4).wait(229).to({y:265,alpha:0},9,cjs.Ease.quintIn).to({_off:true},1).wait(45));
+	this.timeline.addTween(cjs.Tween.get(this.instance_6).wait(229).to({y:265,alpha:0},9,cjs.Ease.quintIn).to({_off:true},1).wait(45));
 
 	// BKGD skew box
-	this.instance_5 = new lib.BKGDskewbox();
-	this.instance_5.setTransform(498.8,349,1,1,0,0,0,613.8,0);
-	this.instance_5.alpha = 0;
-	this.instance_5._off = true;
-
-	this.timeline.addTween(cjs.Tween.get(this.instance_5).wait(239).to({_off:false},0).to({y:249,alpha:1},23,cjs.Ease.quintOut).wait(22));
-
-	// CTA skew box
-	this.instance_6 = new lib.CTAskewbox();
-	this.instance_6.setTransform(200,161,1,1,0,0,0,50,0);
-	this.instance_6.alpha = 0;
-	this.instance_6._off = true;
-
-	this.timeline.addTween(cjs.Tween.get(this.instance_6).wait(246).to({_off:false},0).to({y:61,alpha:1},23,cjs.Ease.quintOut).wait(15));
-
-	// PIC2
-	this.instance_7 = new lib.PIC2();
-	this.instance_7.setTransform(150,125);
+	this.instance_7 = new lib.BKGDskewbox();
+	this.instance_7.setTransform(498.8,349,1,1,0,0,0,613.8,0);
 	this.instance_7.alpha = 0;
 	this.instance_7._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_7).wait(119).to({_off:false},0).to({alpha:1},9).to({_off:true},149).wait(7));
+	this.timeline.addTween(cjs.Tween.get(this.instance_7).wait(239).to({_off:false},0).to({y:249,alpha:1},23,cjs.Ease.quintOut).wait(22));
+
+	// PIC2
+	this.instance_8 = new lib.PIC2();
+	this.instance_8.setTransform(150,125);
+	this.instance_8.alpha = 0;
+	this.instance_8._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_8).wait(119).to({_off:false},0).to({alpha:1},9).to({_off:true},143).wait(13));
 
 	// PIC1
-	this.instance_8 = new lib.PIC1();
-	this.instance_8.setTransform(150,125);
+	this.instance_9 = new lib.PIC1();
+	this.instance_9.setTransform(150,125);
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_8).to({_off:true},128).wait(156));
+	this.timeline.addTween(cjs.Tween.get(this.instance_9).to({_off:true},128).wait(156));
 
 	this._renderFirstFrame();
 
 }).prototype = p = new lib.AnMovieClip();
-p.nominalBounds = new cjs.Rectangle(-56,124.5,1168.7,349.5);
+p.nominalBounds = new cjs.Rectangle(-56,125,1168.7,349);
 // library properties:
 lib.properties = {
 	id: '0957C5EC3B874C629166840920B09671',
@@ -612,7 +636,7 @@ lib.properties = {
 	color: "#999999",
 	opacity: 1.00,
 	manifest: [
-		{src:"images/UWM_cardio_300x250_atlas_1.jpg?1678840215988", id:"UWM_cardio_300x250_atlas_1"}
+		{src:"images/UWM_cardio_300x250_atlas_1.jpg?1678915972667", id:"UWM_cardio_300x250_atlas_1"}
 	],
 	preloads: []
 };
